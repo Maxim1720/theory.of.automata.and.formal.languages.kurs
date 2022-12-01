05f31602c60fd548afde838608929acf053eb898
maksim1708 <musiev_maksim@mail.ru>
2022-12-02 03:05:18 +0400

    to create new architecture

---
+++ src/ru/state/transitor/NumberTransitor.java
@@ -0,0 +1,39 @@
+package ru.state.transitor;
+
+import ru.Reader;
+import ru.handler.NumberLexemHandler;
+import ru.state.State;
+import ru.state.StateType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class NumberTransitor {
+
+    private final HashMap<State, Function<Character, Boolean>> stateFunctionHashMap;
+
+    public NumberTransitor(HashMap<State, Function<Character, Boolean>> stateFunctionHashMap){
+        this.stateFunctionHashMap = stateFunctionHashMap;
+    }
+
+    public Reader transit(Reader reader){
+        for(Map.Entry<State, Function<Character, Boolean>> e: stateFunctionHashMap.entrySet()){
+            if(e.getValue().apply(reader.getCurrent())){
+                return e.getKey().transit(reader);
+            }
+        }
+
+        if (!Character.isWhitespace(reader.getCurrent())
+                && !reader.currentIsDelimiter()){
+            reader.add();
+            reader.setStateType(StateType.ERR);
+        }
+        else {
+            new NumberLexemHandler().handle(reader.getBuffer());
+        }
+
+        return reader;
+    }
+
+}
